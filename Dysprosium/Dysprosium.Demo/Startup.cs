90e05662d0514f5f3a025e674afbc68c568cee5b
Trojan, Piotr <piotr.trojan@dieboldnixdorf.com>
2019-10-29 12:13:54 +0100

    Two middlewares pipeline

--- Dysprosium/Dysprosium.Demo/Startup.cs
+++ Dysprosium/Dysprosium.Demo/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Owin;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
@@ -11,6 +12,13 @@ namespace Dysprosium.Demo
     {
         public static void Configuration(IAppBuilder app)
         {
+            app.Use(async (ctx, next) =>
+            {
+                Debug.WriteLine($"Incoming Request: {ctx.Request.Path}");
+                await next();
+                Debug.WriteLine($"Outgoing Request: {ctx.Request.Path}");
+            });
+
             app.Use(async (ctx, next) =>
             {
                 await ctx.Response.WriteAsync("<html><head></head><body><h1>Hello World!</h1></body></html>");

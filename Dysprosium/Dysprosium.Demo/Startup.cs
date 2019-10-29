3f376ac7bcfe298f9badd2318e86b3ceb94afce9
Trojan, Piotr <piotr.trojan@dieboldnixdorf.com>
2019-10-29 12:09:38 +0100

    Simple OWIN Pipeline example

---
+++ Dysprosium/Dysprosium.Demo/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Dysprosium.Demo
+{
+    public class Startup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+            app.Use(async (ctx, next) =>
+            {
+                await ctx.Response.WriteAsync("<html><head></head><body><h1>Hello World!</h1></body></html>");
+            });
+
+        }
+    }
+}
